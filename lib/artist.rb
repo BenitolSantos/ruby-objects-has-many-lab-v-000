bf77d05c48359e3555bf4316d90685256e8d2647
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-07 17:11:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,6 @@ class Artist
   end
 
   def add_song(song)
-    binding.pry
     song.artist = self #it was expecting the artist to be returned along
     #with the song being added.
     @songs << song
