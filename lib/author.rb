f474a72a2035bf0b0f9c67bc422f75d22d0e6adb
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-07 18:01:44 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,13 +11,13 @@ class Author
   end
 
   def add_post(post)
-    post = Post.new #setting the parameter equal to a new Post class instance
+    post = Post.new(post) #setting the parameter equal to a new Post class instance
     post.author = self #setting the author of that instance equal to self,
     #Author Class Instance
     @posts << post
   end
 
-  
+
 
 
 
